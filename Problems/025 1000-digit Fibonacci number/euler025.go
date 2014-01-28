ec9c2a7ed2800c01de25add55bacaced27212034
John G. Beckett <jgbeckett@gmail.com>
2014-01-28 14:49:30 +0100

    Add solution to problem 025

---
+++ Problems/025 1000-digit Fibonacci number/euler025.go
@@ -0,0 +1,66 @@
+/*
+The Fibonacci sequence is defined by the recurrence relation:
+
+Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+Hence the first 12 terms will be:
+
+F1 = 1
+F2 = 1
+F3 = 2
+F4 = 3
+F5 = 5
+F6 = 8
+F7 = 13
+F8 = 21
+F9 = 34
+F10 = 55
+F11 = 89
+F12 = 144
+The 12th term, F12, is the first term to contain three digits.
+
+What is the first term in the Fibonacci sequence to contain 1000 digits?
+*/
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"time"
+)
+
+var p = fmt.Println
+var pf = fmt.Printf
+
+// Used to time other functions to compare speed
+func timeTrack(start time.Time, name string) {
+	elapsed := time.Since(start)
+	fmt.Printf("%s took %s \n", name, elapsed)
+}
+
+func findFactorialGreaterThanNumDigits(num int) int {
+	defer timeTrack(time.Now(), "findFactorialGreaterThanNumDigits()") // Timer function
+
+	a := big.NewInt(1)
+	b := big.NewInt(1)
+	temp := big.NewInt(1)
+	zero := big.NewInt(0)
+	iteration := 2 // corresponding to the value of 'b'
+
+	// steps to follow for iterations
+	for len(b.String()) < num {
+		temp.Add(a, zero)
+		// p("temp: ", temp)
+		a.Add(b, zero)
+		// p("a: ", a)
+		b.Add(temp, b)
+		// p("b: ", b)
+		iteration++
+		// p("Iteration ", iteration, " = ", b)
+		// p("a: ", a, " ,b: ", b, " iter: ", iteration)
+	}
+	return iteration
+}
+
+func main() {
+	p(findFactorialGreaterThanNumDigits(1000))
+}
